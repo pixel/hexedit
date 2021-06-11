pixel/hexedit

commit b622084ac5150788ee8efcae627e8ae40a7faa44

Implement SigWinch resize handling

display.c
@@ -100,9 +100,29 @@ int computeCursorXPos(int cursor, int hexOrAscii)
 /*******************************************************************************/
 /* Curses functions */
 /*******************************************************************************/
+static void handleSigWinch(int sig)
+{
+  /*Close and refresh window to get new size*/
+  endwin();
+  refresh();
+
+  /*Reset to trigger recalculation*/
+  lineLength = 0;
+
+  clear();
+  initDisplay();
+  readFile();
+  display();
+}
+
 void initCurses(void)
 {
+  struct sigaction sa;
   initscr();
+
+  memset(&sa, 0, sizeof(struct sigaction));
+  sa.sa_handler = handleSigWinch;
+  sigaction(SIGWINCH, &sa, NULL);
 
 #ifdef HAVE_COLORS
   if (colored) {
@@ -115,6 +135,11 @@ void initCurses(void)
   init_pair(4, COLOR_BLUE, COLOR_YELLOW); /* current cursor position*/
 #endif
 
+  initDisplay();
+}
+
+void initDisplay(void)
+{
   refresh();
   raw();
   noecho();
@@ -138,13 +163,13 @@ void initCurses(void)
       if (lineLength == 0) DIE("%s: term is too small (width)\n");
     } else {
       if (computeLineSize() > COLS)
-	DIE("%s: term is too small (width) for selected line length\n");
+        DIE("%s: term is too small (width) for selected line length\n");
     }
     page = lineLength * (LINES - 1);
   }
   colsUsed = computeLineSize();
-  buffer = malloc(page);
-  bufferAttr = malloc(page * sizeof(*bufferAttr));
+  buffer = realloc(buffer,page);
+  bufferAttr = realloc(bufferAttr,page * sizeof(*bufferAttr));
 }
 
 void exitCurses(void)

hexedit.h
@@ -148,6 +148,7 @@ int set_base(INT loc);
 /* Curses functions declaration */
 /*******************************************************************************/
 void initCurses(void);
+void initDisplay(void);
 void exitCurses(void);
 void display(void);
 void displayLine(int offset, int max);

commit 39983ad1b6b7a5a83271af414db00586ee766c35

Move SIGWINCH handling from handler to NCURSES
Use NCURSES internal SIGWINCH handling to generate KEY_RESIZE.
Use KEY_RESIZE to trigger window resize instead of SIGWINCH.

This fixes a crash on some platforms during resize introduced
in b622084ac - it's not safe to allocate or reallocate memory
during a signal handler.

fixes #51

display.c
@@ -100,29 +100,11 @@ int computeCursorXPos(int cursor, int hexOrAscii)
 /*******************************************************************************/
 /* Curses functions */
 /*******************************************************************************/
-static void handleSigWinch(int sig)
-{
-  /*Close and refresh window to get new size*/
-  endwin();
-  refresh();
-
-  /*Reset to trigger recalculation*/
-  lineLength = 0;
-
-  clear();
-  initDisplay();
-  readFile();
-  display();
-}
 
 void initCurses(void)
 {
   struct sigaction sa;
   initscr();
-
-  memset(&sa, 0, sizeof(struct sigaction));
-  sa.sa_handler = handleSigWinch;
-  sigaction(SIGWINCH, &sa, NULL);
 
 #ifdef HAVE_COLORS
   if (colored) {

interact.c
@@ -341,9 +341,21 @@ int key_to_function(int key)
 
   switch (key)
     {
-    case ERR:
     case KEY_RESIZE:
-      /*no-op*/
+      /*Close and refresh window to get new size*/
+      endwin();
+      refresh();
+
+      /*Reset to trigger recalculation*/
+      lineLength = 0;
+
+      clear();
+      initDisplay();
+      readFile();
+      display();
+      break;
+
+    case ERR:
       break;
 
     case KEY_RIGHT: